3a311c15cea4b142612e43e4bd949b48422a1111
ashok3805 <ashokkumarnthangam@gmail.com>
2025-06-11 22:45:25 +0530

    updated

---
+++ Assignment_19/Basic_Programs.groovy
@@ -0,0 +1,86 @@
+package Assignment_19
+
+//Count vowels
+//Scanner sc = new Scanner(System.in)
+//String s = sc.nextLine()
+//count = 0
+//for(int i=0;i<s.length();i++) {
+//	char ch = s.charAt(i)
+//	if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u') {
+//		count++
+//	}
+//}
+//println(count)
+
+
+//reverse a string
+//str = "ashokkumar"
+//for(int i=str.length()-1;i>=0;i--) {
+//	print str.charAt(i);
+//}
+
+//Check if the number is prime 
+//Scanner sc = new Scanner(System.in)
+//n = sc.nextInt()
+//i=2 
+//for(;i<n;i++) {
+//	if(n%i==0) {
+//		println("Not a prime number")
+//		break
+//	}
+//}
+//if(i==n)
+//	println("Prime Number")
+
+//Remove duplicates from a list
+//def arr = [1,2,2,3,4,4,5,6,6]
+//arr = arr.unique()
+//def res = []
+//for(i=0;i<arr.size();i++) {
+//	if(!res.contains(arr[i])) {
+//		res << arr[i]
+//	}
+//}
+//println arr
+
+//Find common element in two list
+//def l1 = [1,2,3,4,5]
+//def l2 = [2,6,5,4,9]
+//def l = []
+//for(i=0;i<l1.size();i++) {
+//	if(l2.contains(l1[i]))
+//		l<<l1[i]
+//}
+//print l
+
+//Anagram
+//def str1 = "listen"
+//def str2 = "silent"
+//if(str1.toList().sort()==str2.toList().sort())
+//	println("Anagram")
+//else
+//	println("Not a Anagram")
+
+//fibonacci serious
+//n = 10
+//a=0
+//b=1
+//print"$a $b "
+//for(i=2;i<n;i++) {
+//	c = a+b
+//	print (c+" ")
+//	a=b
+//	b=c
+//}
+
+//String Palindrome
+//str1 = "madam"
+//str2 = ""
+//for(i=str1.length()-1;i>=0;i--) {
+//	str2 += str1.charAt(i)
+//}
+//println str2
+//if(str1.equals(str2))
+//	println"Palindrome"
+//else
+//	println"Not a Palindrome"
